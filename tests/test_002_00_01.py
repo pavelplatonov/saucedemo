4d5264c5ae3a5921d5131782161bce9327fecf34
zastasja <anastasia_zh@bk.ru>
2022-11-13 20:49:37 +0300

    починила подсчет карточек

--- tests/test_002_00_01.py
+++ tests/test_002_00_01.py
@@ -4,7 +4,6 @@ from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 from pages.locators import CatalogPageLocators
 
-
 link = "https://www.saucedemo.com/"
 link_2 = "https://www.saucedemo.com/inventory.html"
 
@@ -15,19 +14,17 @@ link_2 = "https://www.saucedemo.com/inventory.html"
 def test_login_standard_user(browser):
     page = LoginPage(browser, link)
     page.open_page()
-    page.login_standard_user()
-    page.enter_valid_password()
-    page.enter_valid_password()
-    page.click_login_btn()
+    page.full_login_standard_user()
     page.should_be_current_page(link_2)
 
     # count catalog cards
-    catalogue = browser.find_element(*CatalogPageLocators.CATALOGUE_LIST)
+    browser.implicitly_wait(30)
+    catalogue = browser.find_elements(*CatalogPageLocators.CATALOGUE_LIST)
     count = len(catalogue)
-    assert count == 6, 'Общее количество товаро не соответсвует 6'
+    assert count == 6, 'Общее количество товаров не соответсвует 6'
 
-    # scrolling keyboard
-    browser.find_element(*CatalogPageLocators.CART_BTN).send_keys(Keys.END)
-    time.sleep(2)
-    browser.find_element(*CatalogPageLocators.FOOTER_TEXT)
-    time.sleep(2)
+    # # scrolling keyboard
+    # browser.find_element(*CatalogPageLocators.CART_BTN).send_keys(Keys.END)
+    # time.sleep(2)
+    # browser.find_element(*CatalogPageLocators.FOOTER_TEXT)
+    # time.sleep(2)

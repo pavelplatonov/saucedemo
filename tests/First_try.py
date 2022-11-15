52c6d62c3960038f5e98da2580d1f8da1beaef72
Karina Kulakova <karina.kulakova83@gmail.com>
2022-11-15 21:58:56 +0200

    commit after reverts

---
+++ tests/First_try.py
@@ -0,0 +1,12 @@
+import time
+from pages.login_page import LoginPage
+
+link = "https://www.saucedemo.com/"
+
+def test_login(browser):
+    page = LoginPage (browser, link)
+    page.open_login_page()
+    page.login_standard_user()
+    page.enter_valid_password()
+    page.click_login_btn()
+    time.sleep(2)

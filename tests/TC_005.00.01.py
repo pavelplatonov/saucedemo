63d57698d4b8bf45d0277b6701a9642148a69e0e
Karina Kulakova <karina.kulakova83@gmail.com>
2022-11-17 11:33:19 +0200

    login commit

--- tests/TC_005.00.01.py
+++ tests/TC_005.00.01.py
@@ -1,36 +1,15 @@
 import time
+from pages.login_page import LoginPage
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+link = "https://www.saucedemo.com/"
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://www.saucedemo.com/")
 
-def test_title():
-    title_from_site = driver.title
-
-    assert title_from_site == "Swag Labs"
-
-def test_login_form():
-
-    driver.find_element(By.CSS_SELECTOR, 'input#user-name').send_keys('standard_user')
-    driver.find_element(By.XPATH, "//input[contains(@placeholder, 'Password')]").send_keys('secret_sauce')
-    driver.find_element(By.CSS_SELECTOR, 'input#login-button').click()
-
-    assert driver.current_url == "https://www.saucedemo.com/inventory.html", "We are on another page!!!"
-    time.sleep(2)
-
-# def click_on_icon():
-#     driver.find_element(By.XPATH, "//a[@id='item_4_title_link']").click()
-#     time.sleep(2)
-
-    # icon_button = driver.find_element((By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']"))
-    # icon_button.click()
-
-    driver.quit()
-
-    # class ProductsOnPage:
-    #     SauceLabsBackpack = (By.CSS_SELECTOR, "a#item_4_title_link")
+def test_login(browser):
+    page = LoginPage (browser, link)
+    page.open_login_page()
+    page.login_standard_user()
+    page.enter_valid_password()
+    page.click_login_btn()
+    time.sleep(1)
 
 

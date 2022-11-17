f2b65fad622d9cfcc2fb4484bd889beddada82c9
Pavel Platonov <pavelplaton@gmail.com>
2022-11-17 23:18:15 +0300

    настройка конфтеста для работы экшена

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 
 @pytest.fixture(scope="session")
 def browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1600,1080")
+    options.headless = True
     browser = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
+        service=ChromeService(ChromeDriverManager().install()), options=options
     )
-    browser.maximize_window()
     yield browser
     browser.quit()

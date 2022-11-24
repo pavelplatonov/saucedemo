3b55473f1f731c3c9aa58f0d59c9ac97e4a276e0
Alexander_Dulalaev <camel1985@mail.ru>
2022-11-24 09:02:48 +0300

    update CartPage class

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver import Keys
-from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import CartPageLocators
 from .locators import CatalogPageLocators
 from .locators import LoginPageLocators
@@ -14,48 +14,7 @@ performance_glitch_user = "performance_glitch_user"
 valid_password = "secret_sauce"
 
 
-class CartPage(BasePage):
-    def open_login_page(self):
-        self.open_page()
-        self.should_be_current_page(link)
-
-    def login_standard_user(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_USER, valid_user)
-
-    def login_problem_user(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_USER, problem_user)
-
-    def login_locked_out_user(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_USER, locked_out_user)
-
-    def login_performance_glitch_user(self):
-        self.keyboard_input(
-            *LoginPageLocators.LOGIN_USER, performance_glitch_user
-        )
-
-    def login_invalid_user(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_USER, "admin")
-
-    def enter_valid_password(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_PASSWORD, valid_password)
-
-    def enter_invalid_password(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_PASSWORD, "%password")
-
-    def click_login_btn(self):
-        self.click_element(*LoginPageLocators.LOGIN_BTN)
-
-    def click_enter(self):
-        self.keyboard_input(*LoginPageLocators.LOGIN_PASSWORD, Keys.RETURN)
-
-    def add_bike_light(self):
-        self.click_element(*CatalogPageLocators.BTN_ADD_BIKE_LIGHT)
-
-    def go_to_cart(self):
-        self.click_element(*CatalogPageLocators.CART_BTN)
-
-    def go_to_bike_light_from_basket(self):
-        self.click_element(*CatalogPageLocators.IN_BASKET)
+class CartPage(LoginPage):
 
     def add_fleece_jacket_to_cart(self):
         self.click_element(*CatalogPageLocators.BTN_ADD_FLEECE_JACKET)
